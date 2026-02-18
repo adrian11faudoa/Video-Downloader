d6e0c056deb100a855b3b4b1d3e24040b2bfbeb9
Adrian11Faudoa <afa1823@gmail.com>
2026-02-17 21:50:16 -0600

    Update Code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import yt_dlp
 import threading
 import os
 
-
+# Download Function
 def download():
     url = url_entry.get().strip()
     mode = download_type.get()
@@ -50,7 +50,7 @@ def download():
     threading.Thread(target=run).start()
 
 
-
+# Progress Hook
 def progress_hook(d):
     if d["status"] == "downloading":
         percent = d.get("_percent_str", "0%").strip()
@@ -66,7 +66,7 @@ def browse_folder():
         download_path.set(folder)
 
 
-# UI 
+# UI Setup
 root = tk.Tk()
 root.title("Video / Audio Downloader")
 root.geometry("500x300")
@@ -75,23 +75,19 @@ root.resizable(False, False)
 tk.Label(root, text="Video URL").pack(pady=5)
 url_entry = tk.Entry(root, width=60)
 url_entry.pack()
-
 download_type = tk.StringVar(value="video")
+
 
 frame = tk.Frame(root)
 frame.pack(pady=10)
-
 tk.Radiobutton(frame, text="Video", variable=download_type, value="video").pack(side="left", padx=10)
 tk.Radiobutton(frame, text="Audio (MP3)", variable=download_type, value="audio").pack(side="left", padx=10)
-
 download_path = tk.StringVar()
 
 path_frame = tk.Frame(root)
 path_frame.pack(pady=10)
-
 tk.Entry(path_frame, textvariable=download_path, width=40).pack(side="left")
 tk.Button(path_frame, text="Browse", command=browse_folder).pack(side="left", padx=5)
-
 tk.Button(root, text="Download", command=download, bg="#4CAF50", fg="white").pack(pady=15)
 
 status_label = tk.Label(root, text="")
